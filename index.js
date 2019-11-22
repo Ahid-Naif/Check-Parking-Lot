9a1dec8beaafc4874833c974859d7b73b6137ce3
unknown <ahedqaood19@gmail.com>
2019-11-22 23:38:14 +0300

    change IR sesors pin numbers

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ let Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
 let redLED1 = new Gpio(4, 'out');
 let greenLED1 = new Gpio(27, 'out');
 //use GPIO pin 23 as input, and 'both' on/off status should be handled
-let ir1 = new Gpio(22, 'in', 'both');
+let ir1 = new Gpio(14, 'in', 'both');
 
 let redLED2 = new Gpio(17, 'out');
 let greenLED2 = new Gpio(22, 'out');
 //use GPIO pin 23 as input, and 'both' on/off status should be handled
-let ir2 = new Gpio(24, 'in', 'both');
+let ir2 = new Gpio(15, 'in', 'both');
 
 let loopInterval = setInterval(loop, 1000); //run the loop function every 1 second
 
