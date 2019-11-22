142d1bc10d73004440c96745a38250f12f24bffa
unknown <ahedqaood19@gmail.com>
2019-11-22 22:59:39 +0300

    turn red and green LEDs depending on IR 1 and IR 2 states (ON/OFF)

--- index.js
+++ index.js
@@ -2,34 +2,24 @@ let Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
 
 let redLED1 = new Gpio(4, 'out');
 let greenLED1 = new Gpio(27, 'out');
+//use GPIO pin 23 as input, and 'both' on/off status should be handled
+let ir1 = new Gpio(23, 'in', 'both');
 
 let redLED2 = new Gpio(17, 'out');
 let greenLED2 = new Gpio(22, 'out');
+//use GPIO pin 23 as input, and 'both' on/off status should be handled
+let ir2 = new Gpio(24, 'in', 'both');
 
 let loopInterval = setInterval(loop, 1000); //run the loop function every 1 second
 
-function loop() {
-    if (redLED1.readSync() === 0) { 
-        redLED1.writeSync(1);
-    } else {
-        redLED1.writeSync(0);
-    }
+ir1.watch(function (err, value) {
+    // watch for IR 1 status changes
+    redLED1.writeSync(value);
+    greenLED1.writeSync(!value);
+});
 
-    if (greenLED1.readSync() === 0) { 
-        greenLED1.writeSync(1);
-    } else {
-        greenLED1.writeSync(0);
-    }
-
-    if (redLED2.readSync() === 0) { 
-        redLED2.writeSync(1);
-    } else {
-        redLED2.writeSync(0);
-    }
-
-    if (greenLED2.readSync() === 0) { 
-        greenLED2.writeSync(1);
-    } else {
-        greenLED2.writeSync(0);
-    }
-}
+ir2.watch(function (err, value) {
+    // watch for IR 1 status changes
+    redLED2.writeSync(value);
+    greenLED2.writeSync(!value);
+});
