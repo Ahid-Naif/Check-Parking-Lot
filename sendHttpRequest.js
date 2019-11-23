d6871d7d6940c41efdd213aebddd0045c97c1262
unknown <ahedqaood19@gmail.com>
2019-11-23 04:24:00 +0300

    configure heroku

--- sendHttpRequest.js
+++ sendHttpRequest.js
@@ -3,8 +3,8 @@ const axios = require('axios')
 function post(parking_lot_status){
     axios({
         method: 'post',
-        url: 'http://localhost:8080/parking_lot_status',
-        // url: 'https://guarded-temple-62222.herokuapp.com/id',
+        // url: 'http://localhost:8080/parking_lot_status',
+        url: 'https://rocky-cove-98552.herokuapp.com/parking_lot_status',
         data: parking_lot_status,
         config: { headers: {'Content-Type': 'multipart/form-data' }}
         })
